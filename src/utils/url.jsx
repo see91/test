5c5b6d9b76fe4258c064e03bc027fd0f55f67aac
see91 <wangqizhen@fancyfuture.com.cn>
2020-07-31 16:53:01 +0800

    🔨 fix

--- src/utils/url.jsx
+++ src/utils/url.jsx
@@ -1,5 +1,6 @@
-
+/**
+ * 未应用   现使用webpack代理
+ */
 // http://192.168.1.137:8888
 // https://em.tengwanweigu.com
-export const baseUrl = 'https://em.tengwanweigu.com';
 // export const baseUrl = 'http://192.168.1.137:8888';
